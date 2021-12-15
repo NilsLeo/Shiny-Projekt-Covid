e43cdfd87e44f4e52b67473e47c0b017345effec
s0578579 <s0578579@HTW-Berlin.de>
2021-12-15 21:27:17 +0100

    Erste Schritte

--- App.R
+++ App.R
@@ -2,36 +2,67 @@ library(shiny)
 library(shinythemes)
 library(datasets)
 
-# Quelle https://github.com/robert-koch-institut
+# Quellen
 
-Verteilung <- read.csv2("C:/Users/Nils/My Drive/Documents/HTW/WiSe 2021 22/Statistik/Shiny/RKI Datensätze/bezirkstabelle.csv")
-VerteilungohneBezirke <- data.frame(Verteilung$Fallzahl, Verteilung$Differenz, Verteilung$Inzidenz, Verteilung$Genesen)
-ui <- fluidPage(
-  navbarPage(
-    # theme = "cerulean"
-    "Die SARS-CoV-2-Pandemie",
-    tabPanel("Fallzahlen",
-             sidebarPanel(
-               selectInput("region", "Region:", 
-                           choices=colnames(WorldPhones)),
-             )
-             
-    ), # sidebarPanel
-    mainPanel(
-      plotOutput("phonePlot")  
+# inspiriert durch: https://shiny.rstudio.com/gallery/telephones-by-region.html
+
+WP <- WorldPhones
+
+# Einlesen, Invertierung, neubenennung der Zeilen/ Spalten, Entfernen der Spalte"Berlin"
+CSV <- read.csv2("https://www.berlin.de/lageso/_assets/gesundheit/publikationen/corona/bezirkstabelle.csv")
+df <- data.frame(CSV)
+df <- t(df)
+colnames(df) <- df[1,]
+df <- df[-1,]
+df <- df[,-13]
+
+# Neue tabelle mit einer Zeile für die heutigen Fallzahlen
+heutigeFallzahl <- df[1,]
+heutigeFallzahl <- t(heutigeFallzahl)
+heutigeFallzahl <- cbind( new_col = format(Sys.time(), "%d-%m-%Y"), heutigeFallzahl)
+datumFallzahl <- heutigeFallzahl[,1]
+rownames(heutigeFallzahl) <- datumFallzahl
+heutigeFallzahl <- heutigeFallzahl[,-1]
+heutigeFallzahl <-t(heutigeFallzahl)
+rownames(heutigeFallzahl) <- datumFallzahl
+
+ # Tabelle mit den Fallzahlen für alle beobachteten Tage
+Fallzahlen <- heutigeFallzahl
+
+ui <- # Use a fluid Bootstrap layout
+  fluidPage(    
+    
+    # Give the page a title
+    titlePanel("Fallzahlen je Bezirk"),
+    
+    # Generate a row with a sidebar
+    sidebarLayout(      
+      
+      # Define the sidebar with one input
+      sidebarPanel(
+        selectInput("bezirk", "Bezirk:", 
+                    choices=colnames(Fallzahlen)),
+      ),
+      
+      # Create a spot for the barplot
+      mainPanel(
+        plotOutput("fallzahlenPlot")  
+      )
+      
     )
   )
-)
 
 
 server <- function(input, output) {
-  output$phonePlot <- renderPlot({
+  
+  # Fill in the spot we created for a plot
+  output$fallzahlenPlot <- renderPlot({
     
     # Render a barplot
-    barplot(WorldPhones[,input$region]*1000, 
-            main=input$region,
-            ylab="Number of Telephones",
-            xlab="Year")
+    barplot(Fallzahlen[,input$bezirk], 
+            main=input$bezirk,
+            ylab="Fallzahlen",
+            xlab="Datum")
   })
 }
 shinyApp(ui = ui, server = server)
